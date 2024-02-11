a8f5848f3dfd071b9cc75332072ce46c75ac0939
Rybakova Nastya <k.anastasija.o@gmail.com>
2024-02-11 17:11:12 +0300

    Доздача ДЗ

--- howOldAreYou.js
+++ howOldAreYou.js
@@ -1,3 +1,20 @@
-let number = 20;
-number %= 2;
-console.log(number);
+let age = prompt("Введіть будь ласка Ваш вік")
+//let age = 44;
+let counter = age;
+let word = "";
+if (age < 0){
+    console.log("Введіть додатнє число!")
+}
+else if(counter >=10 && counter<=20){
+    word = "років";
+} else {
+    counter = age % 10;
+    if (counter === 1){
+        word = "рік";
+    } else if(counter >= 2 && counter <= 4){
+        word = "роки";
+    } else {
+        word = "років";
+    }
+}
+console.log(age + " " + word);
